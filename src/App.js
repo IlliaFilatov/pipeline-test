2d9641c19db27bab6f6354b49520c8bfba6f09fa
Illia Filatov <illia.filatov.hn@gmail.com>
2021-09-02 19:30:58 +0300

    version & design update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
+import { useState, useCallback } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const VERSION = 1.1;
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const increment = useCallback(() => {
+    setCounter(counter + 1);
+  }, [counter]);
+
+  const decrement = useCallback(() => {
+    setCounter(counter - 1);
+  }, [counter]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="App-border">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div class="App-button_container">
+            <button class="App-button" onClick={increment}>Increment</button>
+            <button class="App-button" onClick={decrement}>Decrement</button>
+          </div>
+          <p className="App-link">
+            {`Current value: ${counter}`}
+          </p>
+          <p className="App-link">
+            {`Version: ${VERSION.toFixed(1)}`}
+          </p>
+        </div>
       </header>
     </div>
   );
